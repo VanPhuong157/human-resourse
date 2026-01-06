0a8a085691e5126392f1ce0e7a4fff3b39a0158b
nvp01507@gmail.com <nvp01507@gmail.com>
2026-01-06 13:48:37 +0700

    update root api

--- SmartHR/src/api/rootApi.js
+++ SmartHR/src/api/rootApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const baseUrl = 'http://localhost:5158'
-const baseUrl = 'http://192.168.10.111:5158'
+const baseUrl = 'https://etm-azcvdeftdeamfsdx.japaneast-01.azurewebsites.net'
 const rootApi = axios.create({
   baseURL: `${baseUrl}/api`,
   headers: {
