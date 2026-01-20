51f1189bbf44bbb5da1c7b74bd3f213e27847ead
nvp01507@gmail.com <nvp01507@gmail.com>
2026-01-20 14:41:29 +0700

    Fix Kestrel port binding

--- backup-portal-api/Sep490_G49_Api/Sep490_G49_Api/Program.cs
+++ backup-portal-api/Sep490_G49_Api/Sep490_G49_Api/Program.cs
@@ -24,6 +24,7 @@ using Repository.WorkFlows;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://*:8080");
 
 // Add services to the container.
 builder.Services.AddControllers()
